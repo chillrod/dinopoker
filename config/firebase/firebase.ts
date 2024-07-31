ed7f653d5ccb93bfac43ee47d8ad038cefc34f1a
chillrod <chillrod77@gmail.com>
2024-07-31 12:56:11 -0300

    fix: hide env variables

--- config/firebase/firebase.ts
+++ config/firebase/firebase.ts
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5OZzYbBmxvnGxTj-m3U98S1o6S0GQk0Y",
-  authDomain: "dinopokerapp.firebaseapp.com",
-  databaseURL: "https://dinopokerapp-default-rtdb.firebaseio.com",
-  projectId: "dinopokerapp",
-  storageBucket: "dinopokerapp.appspot.com",
-  messagingSenderId: "524389525362",
-  appId: "1:524389525362:web:31e72c281474b411b9dfeb",
-  measurementId: "G-0ERBZRHF15",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 export const appFirebase = initializeApp(firebaseConfig);
