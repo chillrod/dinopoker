fbfcda13346365bc37a52677cf7792d44a604fa2
chillrod <chillrod77@gmail.com>
2024-08-15 11:39:23 -0300

    feat: added  ? to vote system

--- config/vote-system/vote-system.ts
+++ config/vote-system/vote-system.ts
@@ -1,12 +1,12 @@
 export interface IVoteSystem {
-  voteSystem: number[];
+  voteSystem: [string | number][];
 }
 
 export const VoteSystemOptions: { [key: string]: any } = {
   ["modified-fibonacci"]: {
-    voteSystem: [0.5, 1, 2, 3, 5, 8, 13, 21],
+    voteSystem: ["?", 0.5, 1, 2, 3, 5, 8, 13, 21],
   },
   ["fibonacci"]: {
-    voteSystem: [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89],
+    voteSystem: ["?", 0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89],
   },
 };
